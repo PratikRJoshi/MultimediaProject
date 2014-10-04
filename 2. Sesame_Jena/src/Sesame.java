4fe9da4eb787a440cebda2fe7738608afa8ae6b5
PratikRJoshi <pratikjo@usc.edu>
2014-10-04 15:52:21 -0700

    Got results back from DBpedia

--- 2. Sesame_Jena/src/Sesame.java
+++ 2. Sesame_Jena/src/Sesame.java
@@ -1,12 +1,23 @@
 import java.io.File;
 
+
+
+
+
+
+//import org.openrdf.query.Query;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.sail.nativerdf.NativeStore;
 
+import com.github.jsonldjava.core.RDFDataset.Literal;
+import com.hp.hpl.jena.query.ARQ;
+import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.ResultSetFormatter;
 
@@ -14,28 +25,55 @@ import com.hp.hpl.jena.query.ResultSetFormatter;
 
 public class Sesame {
 	public static void main(String args[]) throws RepositoryException{
-		File file = new File("/Users/student/Desktop/file.txt");
+		/*File file = new File("/Users/student/Desktop/file.txt");
 		Repository repo = new SailRepository(new NativeStore(file));
-		repo.initialize();
+		repo.initialize();*/
 		
-		String ontology_service = "http://ambit.uni-plovdiv.bg:8080/ontology";
-		String endpoint = "otee:Endpoints";
-		String endpointsSparql = 
-		"PREFIX ot:<http://www.opentox.org/api/1.1#>\n"+
-		"PREFIX ota:<http://www.opentox.org/algorithms.owl#>\n"+
-		"PREFIX owl:<http://www.w3.org/2002/07/owl#>\n"+
-		"PREFIX dc:<http://purl.org/dc/elements/1.1/>\n"+
-		"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
-		"PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
-		"PREFIX otee:<http://www.opentox.org/echaEndpoints.owl#>\n"+
-		"select ?url ?title\n"+
-		"where {\n"+
-		"?url rdfs:subClassOf %s.\n"+
-		"?url dc:title ?title.\n"+
-		"}\n";
+		String query =  "PREFIX dbpprop: <http://dbpedia.org/property/>"+
+						"PREFIX dbpedia: <http://dbpedia.org/resource/>"+
+						"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"+
+						"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
+						"PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>"+
+						"SELECT DISTINCT ?university ?name ?city ?latitude ?longitude "+
+						"WHERE{ {"+
+							"?university dbpprop:type dbpedia:Private_university."+
+							"?university dbpprop:state dbpedia:California."+ 
+
+							"OPTIONAL {?university dbpprop:name ?name.}"+
+							"OPTIONAL {?university dbpprop:city ?city.}"+
+							//"OPTIONAL {?university dbpedia-owl:city ?city.}"+
+							"OPTIONAL {?university dbpprop:label ?name.}"+
+							"OPTIONAL {?university rdfs:label ?name.}"+
+							"OPTIONAL {?university geo:lat ?latitude.}"+
+							"OPTIONAL {?university geo:long ?longitude.}"+
+							"OPTIONAL {?university dbpprop:lat ?latitude.}"+
+							"OPTIONAL {?university dbpprop:long ?longitude.}"+
+							"OPTIONAL {?university dbpprop:location ?city.}"+
+							"}}";
 		
-		QueryExecution x = QueryExecutionFactory.sparqlService(ontology_service, String.format(endpointsSparql,endpoint));
-		ResultSet results = x.execSelect();
-		ResultSetFormatter.out(System.out, results);
+		Query q =  QueryFactory.create(query);
+		ARQ.getContext().setTrue(ARQ.useSAX);
+		QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", q);
+		
+		try{
+			ResultSet result = qexec.execSelect();
+//			ResultSetFormatter.out(System.out, result, q);
+		
+			if(qexec.execSelect()!=null)
+				System.out.println("Present");
+			else
+				System.out.println("Absent");
+		
+			while(result.hasNext()) {
+				QuerySolution qs = result.nextSolution();
+				System.out.println(qs.get("?university"));
+			}
+		}
+		finally{
+			qexec.close();
+		}
+//		QueryExecution x = QueryExecutionFactory.sparqlService(ontology_service, String.format(endpointsSparql,endpoint));
+//		ResultSet results = x.execSelect();
+//		ResultSetFormatter.out(System.out, results);
 	}
 }
