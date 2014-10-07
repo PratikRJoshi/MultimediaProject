29a7a3242015ae128287d17b87f076e3027ead6e
PratikRJoshi <pratikjo@usc.edu>
2014-10-06 18:34:27 -0700

    Got results from DBpedia

--- 2. Sesame_Jena/src/Sesame.java
+++ 2. Sesame_Jena/src/Sesame.java
@@ -1,79 +1,77 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
-
-
-
-
-
-//import org.openrdf.query.Query;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.sail.nativerdf.NativeStore;
 
-import com.github.jsonldjava.core.RDFDataset.Literal;
-import com.hp.hpl.jena.query.ARQ;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFormatter;
 
 
 
 public class Sesame {
-	public static void main(String args[]) throws RepositoryException{
-		/*File file = new File("/Users/student/Desktop/file.txt");
-		Repository repo = new SailRepository(new NativeStore(file));
-		repo.initialize();*/
+	public static void main(String args[]) throws RepositoryException, FileNotFoundException{
+		File file = new File("/Users/student/Desktop/outputFile.txt");
+		PrintWriter pw = new PrintWriter(file);
+//		Repository repo = new SailRepository(new NativeStore(file));
+//		repo.initialize();
 		
-		String query =  "PREFIX dbpprop: <http://dbpedia.org/property/>"+
-						"PREFIX dbpedia: <http://dbpedia.org/resource/>"+
-						"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"+
-						"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
-						"PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>"+
-						"SELECT DISTINCT ?university ?name ?city ?latitude ?longitude "+
-						"WHERE{ {"+
-							"?university dbpprop:type dbpedia:Private_university."+
-							"?university dbpprop:state dbpedia:California."+ 
+		String query = "PREFIX dbpprop: <http://dbpedia.org/property/>\n"+
+						"PREFIX dbpedia: <http://dbpedia.org/resource/>\n"+
+						"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
+						"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+						"PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\n"+
+						"SELECT DISTINCT ?university ?name ?city ?latitude ?longitude\n"+ 
+						"WHERE {\n"+
+							"?university dbpprop:type dbpedia:Private_university.\n"+
+							"?university dbpprop:state dbpedia:California.\n"+ 
 
-							"OPTIONAL {?university dbpprop:name ?name.}"+
-							"OPTIONAL {?university dbpprop:city ?city.}"+
-							//"OPTIONAL {?university dbpedia-owl:city ?city.}"+
-							"OPTIONAL {?university dbpprop:label ?name.}"+
-							"OPTIONAL {?university rdfs:label ?name.}"+
-							"OPTIONAL {?university geo:lat ?latitude.}"+
-							"OPTIONAL {?university geo:long ?longitude.}"+
-							"OPTIONAL {?university dbpprop:lat ?latitude.}"+
-							"OPTIONAL {?university dbpprop:long ?longitude.}"+
-							"OPTIONAL {?university dbpprop:location ?city.}"+
-							"}}";
-		
-		Query q =  QueryFactory.create(query);
-		ARQ.getContext().setTrue(ARQ.useSAX);
+        					"OPTIONAL {?university dbpprop:name ?name.}\n"+
+        					"OPTIONAL {?university dbpprop:city ?city.}\n"+
+        					"#OPTIONAL {?university dbpedia-owl:city ?city.}\n"+
+        					"OPTIONAL {?university dbpprop:label ?name.}\n"+
+        					"OPTIONAL {?university rdfs:label ?name.}\n"+
+        					"OPTIONAL {?university geo:lat ?latitude.}\n"+
+        					"OPTIONAL {?university geo:long ?longitude.}\n"+
+        					"OPTIONAL {?university dbpprop:lat ?latitude.}\n"+
+        					"OPTIONAL {?university dbpprop:long ?longitude.}\n"+
+        					"OPTIONAL {?university dbpprop:location ?city.}\n"+  
+						"}";
+
+
+		Query q = QueryFactory.create(query);
 		QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", q);
-		
 		try{
-			ResultSet result = qexec.execSelect();
-//			ResultSetFormatter.out(System.out, result, q);
-		
-			if(qexec.execSelect()!=null)
-				System.out.println("Present");
-			else
-				System.out.println("Absent");
-		
-			while(result.hasNext()) {
-				QuerySolution qs = result.nextSolution();
-				System.out.println(qs.get("?university"));
+			ResultSet results = qexec.execSelect();
+			int counter = 1;
+			while(results.hasNext()){
+				QuerySolution qs = results.nextSolution();
+				System.out.print(counter+". ");
+				System.out.print("\t"+qs.get("?university"));
+				System.out.print("\t"+qs.get("?name"));
+				System.out.print("\t"+qs.get("?city"));
+				System.out.print("\t"+qs.get("?latitude"));
+				System.out.print("\t"+qs.get("?longitude"));
+				System.out.println();
+				pw.write(counter+". ");
+				pw.write("\t"+qs.get("?university"));
+				pw.write("\t"+qs.get("?name"));
+				pw.write("\t"+qs.get("?city"));
+				pw.write("\t"+qs.get("?latitude"));
+				pw.write("\t"+qs.get("?longitude"));
+				pw.write("\n");
+				counter++;
 			}
 		}
 		finally{
 			qexec.close();
 		}
-//		QueryExecution x = QueryExecutionFactory.sparqlService(ontology_service, String.format(endpointsSparql,endpoint));
-//		ResultSet results = x.execSelect();
-//		ResultSetFormatter.out(System.out, results);
 	}
 }
