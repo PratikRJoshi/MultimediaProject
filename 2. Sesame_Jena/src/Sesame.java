0f513a3935ac0a05657b8d7a15d7cff8cf512294
PratikRJoshi <pratikjo@usc.edu>
2014-10-06 22:01:08 -0700

    All the fetched triples added to the Sesame Triple Store

--- 2. Sesame_Jena/src/Sesame.java
+++ 2. Sesame_Jena/src/Sesame.java
@@ -9,8 +9,6 @@ import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.sail.Sail;
 import org.openrdf.sail.inferencer.fc.ForwardChainingRDFSInferencer;
 import org.openrdf.sail.memory.MemoryStore;
-import org.openrdf.sail.nativerdf.NativeStore;
-
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
@@ -18,23 +16,20 @@ import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 
-import java.io.IOException;
-import java.net.URL;
+
 import java.util.List;
 
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.openrdf.OpenRDFException;
+
+import org.openrdf.model.Literal;
 import org.openrdf.model.URI;
-import org.openrdf.model.Value;
+//import org.openrdf.model.Value;
 import org.openrdf.model.ValueFactory;
-import org.openrdf.model.vocabulary.RDFS;
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
-import org.openrdf.rio.RDFFormat;
 
 
 public class Sesame {
@@ -76,6 +71,15 @@ public class Sesame {
 		try{
 			ResultSet results = qexec.execSelect();
 			int counter = 1;
+			
+			Repository repository = getOnMemoryRepository();
+			RepositoryConnection connection = repository.getConnection();
+			
+			String schemaURI = "http://schema.org/";
+			String dummyURI = "http://www.dummy.org/";
+			ValueFactory vFactory = repository.getValueFactory();
+
+			
 			while(results.hasNext()){
 				QuerySolution qs = results.nextSolution();
 				System.out.print(counter+". ");
@@ -87,6 +91,89 @@ public class Sesame {
 				System.out.println();
 				pw.write(counter+". ");
 				pw.write("\t"+qs.get("?university"));
+				
+				String university = (qs.get("?university")==null?"":qs.get("?university").toString());
+				URI subject = vFactory.createURI(university);
+				URI predicate = vFactory.createURI("http://www.w3.org/2000/01/rdf-schema#type");
+				URI object = vFactory.createURI(schemaURI+"CollegeOrUniversity");
+				
+				//add the university type triple to the store
+				connection.add(subject, predicate, object);
+				
+				subject = vFactory.createURI(university);
+				predicate = vFactory.createURI(schemaURI+"location");
+				object = vFactory.createURI(dummyURI+"loc"+counter);
+				
+				//add the dummy location triple to the store
+				connection.add(subject, predicate, object);
+				
+				subject = vFactory.createURI(dummyURI+"loc"+counter);
+				predicate = vFactory.createURI("http://www.w3.org/2000/01/rdf-schema#type");
+				object = vFactory.createURI(schemaURI+"Place");
+				
+				//add the location type triple to the store
+				connection.add(subject, predicate, object);
+				
+				subject = vFactory.createURI(dummyURI+"loc"+counter);
+				predicate = vFactory.createURI(schemaURI+"geo");
+				object = vFactory.createURI(dummyURI+"geo"+counter);
+				
+				//add the dummy geo triple to the store
+				connection.add(subject, predicate, object);
+				
+				subject = vFactory.createURI(dummyURI+"geo"+counter);
+				predicate = vFactory.createURI("http://www.w3.org/2000/01/rdf-schema#type");
+				object = vFactory.createURI(schemaURI+"GeoCoordinates");
+				
+				//add the geo type triple to the store
+				connection.add(subject, predicate, object);
+				
+				subject = vFactory.createURI(dummyURI+"geo"+counter);
+				predicate = vFactory.createURI(schemaURI+"latitude");
+				String latitude = (qs.get("?latitude")==null?"":qs.get("?latitude").toString());
+				Literal lit = vFactory.createLiteral(latitude);
+				
+				//add the latitude triple to the store
+				connection.add(subject, predicate, lit);
+				
+				subject = vFactory.createURI(dummyURI+"geo"+counter);
+				predicate = vFactory.createURI(schemaURI+"longitude");
+				String longitude = (qs.get("?longitude")==null?"":qs.get("?longitude").toString());
+				lit = vFactory.createLiteral(longitude);
+				
+				//add the longitude triple to the store
+				connection.add(subject, predicate, lit);
+				
+				subject = vFactory.createURI(dummyURI+"loc"+counter);
+				predicate = vFactory.createURI(schemaURI+"address");
+				object = vFactory.createURI(dummyURI+"address"+counter);
+				
+				//add the dummy address triple to the store
+				connection.add(subject, predicate, object);
+				
+				subject = vFactory.createURI(dummyURI+"address"+counter);
+				predicate = vFactory.createURI("http://www.w3.org/2000/01/rdf-schema#type");
+				object = vFactory.createURI(schemaURI+"PostalAddress");
+				
+				//add the address type triple to the store
+				connection.add(subject, predicate, object);
+				
+				subject = vFactory.createURI(dummyURI+"address"+counter);
+				predicate = vFactory.createURI(schemaURI+"addressLocality");
+				String addressLocality = (qs.get("?city")==null?"":qs.get("?city").toString());
+				lit = vFactory.createLiteral(addressLocality);
+				
+				//add the city name triple to the store
+				connection.add(subject, predicate, lit);
+				
+				subject = vFactory.createURI(university);
+				predicate = vFactory.createURI(schemaURI+"legalName");
+				String legalName = (qs.get("?name")==null?"":qs.get("?name").toString());
+				lit = vFactory.createLiteral(legalName);
+				
+				//add the university name triple to the store
+				connection.add(subject, predicate, lit);
+				
 				pw.write("\t"+qs.get("?name"));
 				pw.write("\t"+qs.get("?city"));
 				pw.write("\t"+qs.get("?latitude"));
@@ -94,31 +181,16 @@ public class Sesame {
 				pw.write("\n");
 				counter++;
 			}
-		}
-		finally{
-			qexec.close();
-		}
-		
-		String baseURI = "http://schema.org/";
-		
-		try{
-			Repository repository = getOnMemoryRepository();
-			RepositoryConnection connection = repository.getConnection();
-			
-			ValueFactory vFactory = repository.getValueFactory();
-			URI usc = vFactory.createURI(baseURI+"USC");
-			URI type = vFactory.createURI("http://www.w3.org/2000/01/rdf-schema#type");
-			URI college = vFactory.createURI(baseURI+"CollegeOrUniversity");
-			
-			//add the triple to the store
-			connection.add(usc, type, college);
 			
 			//query the triple store to check if the added triple can be retrieved
 			queryRepository(repository);
-		}catch(RepositoryException re){
-			re.getMessage();
+			
 		}
-		
+		finally{
+			qexec.close();
+			pw.close();
+		}
+			
 	}
 	/**
 	 * Creates an on-memory Repository object.
@@ -136,13 +208,17 @@ public class Sesame {
 	static void queryRepository(Repository repository) throws RepositoryException, MalformedQueryException, QueryEvaluationException{
 		RepositoryConnection rConnection = repository.getConnection();
 		String qString = "SELECT ?x ?y WHERE {?x ?p ?y}";
-		TupleQuery tQuery = rConnection.prepareTupleQuery(QueryLanguage.SPARQL, qString);
+		String queryString = "SELECT ?y "
+				+ "WHERE "
+				+ "{ <http://dbpedia.org/resource/Point_Loma_Nazarene_University> <http://schema.org/legalName> ?y."
+				+ " } ";
+		TupleQuery tQuery = rConnection.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
 		TupleQueryResult tqResult = tQuery.evaluate();
 		
 		while(tqResult.hasNext()){
 			BindingSet bindingSet = tqResult.next();
-			Value vX = bindingSet.getValue("x");
-			Value vY = bindingSet.getValue("y");
+//			Value vX = bindingSet.getValue("x");
+//			Value vY = bindingSet.getValue("y");
 			
 			List<String> names = tqResult.getBindingNames();
 			for (String string : names) {
