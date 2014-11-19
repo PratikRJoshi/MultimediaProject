d9ca7970492befe36137c52a0d106d461225c57c
Sathish Srinivasan <ssathish59@gmail.com>
2014-11-19 01:02:15 -0800

    Inital Commit

---
+++ src/edu/usc/csci576/fast/media/browsing/ui/SampleWindow.java
@@ -0,0 +1,16 @@
+package edu.usc.csci576.fast.media.browsing.ui;
+
+/**
+ * @author Sathish Srinivasan
+ */
+public class SampleWindow {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
