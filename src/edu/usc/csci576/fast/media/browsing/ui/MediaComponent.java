ee70a54ad13c3f1679ec52a7277cd44772e94c64
Sathish Srinivasan <ssathish59@gmail.com>
2014-11-29 21:33:12 -0800

    Remove dead code

--- src/edu/usc/csci576/fast/media/browsing/ui/MediaComponent.java
+++ src/edu/usc/csci576/fast/media/browsing/ui/MediaComponent.java
@@ -13,17 +13,13 @@ import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
-import javax.swing.ImageIcon;
 import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 /**
  * @author Sathish Srinivasan
@@ -34,9 +30,7 @@ public class MediaComponent extends JComponent implements MouseListener {
 	private static final long serialVersionUID = 1L;
 	private Media media; 
 	private HashMap<Rectangle, Media> collageMetadata;
-	//private DisplayMedia displayComponent;
 	private BufferedImage image;
-	//private JLabel label;
 	
 	public MediaComponent(Media media, DisplayMedia displayComponent) throws IOException {
 		//this.displayComponent = displayComponent;
@@ -74,24 +68,7 @@ public class MediaComponent extends JComponent implements MouseListener {
 		int numOfFrames = getNumberOfFrames(filePath, fileType);
 		for(int i=0;i<numOfFrames;i++) {
 			image = Collage.getImageWithFrameNumber(filePath, i, frameWidth, frameHeight);
-			
-			/*//Collage.display(image, frameWidth, frameHeight);
-			if(label != null) {
-				this.remove(label);
-			}
-			frame.revalidate();
-			frame.repaint();
-			this.paintChildren(g);
-			ImageIcon icon = new ImageIcon(image);
-			label = new JLabel(icon);
-			this.add(label);*/
-			paintImmediately(0, 0, this.getWidth(), this.getHeight());
-			
-			/*frame.revalidate();
-			frame.repaint();
-			frame.pack();
-			frame.setVisible(true);*/
-			
+			paintImmediately(0, 0, this.getWidth(), this.getHeight());			
 			try {
 				Thread.sleep(1000/30);
 			} catch (InterruptedException e) {
@@ -136,57 +113,16 @@ public class MediaComponent extends JComponent implements MouseListener {
 		return numOfFrames;
 	}
 
-	private byte[] readFromFile(Path filePath) throws IOException {
-		int fileSize = (int) filePath.toFile().length();
-		byte[] fileContents = new byte[fileSize];
-		FileInputStream fis = new FileInputStream(filePath.toFile());
-		fis.read(fileContents);
-		fis.close();
-		return fileContents;
-	}
-
-	private BufferedImage createImage(byte[] fileContents, int frameNumber,
-			MediaType mediaType) throws IOException {
-
-		int offset = 0;
-		int height = MainUI.getHeight(mediaType);
-		int width = MainUI.getWidth(mediaType);
-		int startOffset = height * width * 3 * frameNumber;
-		BufferedImage image = new BufferedImage(width, height,
-				BufferedImage.TYPE_INT_RGB);
-		for (int y = 0; y < height; y++) {
-			offset =  y * width;
-			for (int x = 0; x < width; x++) {
-				byte red = fileContents[startOffset + offset + x];
-				byte green = fileContents[startOffset + offset + x
-						+ (width * height)];
-				byte blue = fileContents[startOffset + offset + x
-						+ (width * height * 2)];
-				int pixelValue = 0xff000000 | ((red & 0xff) << 16)
-						| ((green & 0xff) << 8) | (blue & 0xff);
-				image.setRGB(x, y, pixelValue);
-				offset++;
-			}
-		}
-		if(fileContents.length == 91238400) {
-			Collage.display(image, width, height);
-		}
-		return image;
-	}
-
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if(MediaType.Collage == media.getFileType()) {
 			Point p = new Point(e.getX(), e.getY());
 			for(Rectangle r: collageMetadata.keySet()) {
 				if(r.contains(p)) {
-					//JLabel statusComponent = displayComponent.getStatusComponent();
 					Media m = collageMetadata.get(r);
-					//statusComponent.setText(m.getFilePath().toString());
 					try {
 						displayMedia(m);
 					} catch (IOException e1) {
-						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
 				}
